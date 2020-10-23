abd6a10d783dc07993ccf9a3ed9d6b623b761fde
Hamza HArmouche <58759442+harmouche@users.noreply.github.com>
2020-10-22 22:10:06 -0400

    new directory

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+<Router>
+    <App />
+</Router>
+    , rootElement);
