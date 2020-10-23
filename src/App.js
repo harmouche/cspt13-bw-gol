6ae467cb8bd6858f0f352a6598b5a609e5ba8e92
Hamza HArmouche <58759442+harmouche@users.noreply.github.com>
2020-10-23 00:15:41 -0400

    deploy2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 import Main from './components/Main';
 import Rules from './components/Rules'
 
