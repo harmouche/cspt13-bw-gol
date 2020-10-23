d75715688908198b5ca75f3275c7e8e128ef3ed5
Hamza HArmouche <58759442+harmouche@users.noreply.github.com>
2020-10-22 23:50:20 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router } from "react-router-dom"
 import Main from './components/Main';
+import Rules from './components/Rules'
 
 function App() {
   return(
   <Router>
     <div className="App">
       <Main />
+      <Rules />
     </div>
   </Router>
   );
